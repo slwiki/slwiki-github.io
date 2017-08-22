edfc6aec07cb8c972c4c741f0b3987f0f793c528
slwiki <simranjit.singh2@pentair.com>
2017-08-22 10:48:15 -0700

    Update slwikichrome.bat

--- files/slwikichrome.bat
+++ files/slwikichrome.bat
@@ -1,5 +1,49 @@
-Opening Chrome:)
+@echo off
+setlocal EnableDelayedExpansion
+color A
+echo Opening Chrome:)...
+ping -n 2 127.0.0.1>nul
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo                                     ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo ^|^|                                  ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Initializing.....
+echo ---------------------------------------
+echo ^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|               ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
 @ECHO N|xcopy "N:\Moorpark\Public\slwiki\Chrome.cameyo.exe" "c:\slwiki\" /Y
 start c:\slwiki\Chrome.cameyo.exe
-
 exit
