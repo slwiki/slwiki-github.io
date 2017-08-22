b37525b414ca4cd2e941c8aa33de82faab746a90
slwiki <simranjit.singh2@pentair.com>
2017-08-22 10:19:37 -0700

    chrome fix

--- files/slwikichrome.bat
+++ files/slwikichrome.bat
@@ -1,5 +1,4 @@
-@ECHO off
-
-start N:\Moorpark\Public\slwiki\Chrome.cameyo.exe
+@ECHO N|xcopy "N:\Moorpark\Public\slwiki\Chrome.cameyo.exe" "c:\slwiki\" /Y
+start c:\slwiki\Chrome.cameyo.exe
 
 exit
