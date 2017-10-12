2ff161fc489802d0c3693dfdf666874812c18251
slwiki <simranjit.singh2@pentair.com>
2017-10-12 11:21:37 -0700

    Create shortcuts.bat

---
+++ files/shortcuts.bat
@@ -0,0 +1,51 @@
+@echo off
+setlocal EnableDelayedExpansion
+color A
+echo SlWIki
+echo Creating Shortcuts:)...
+ping -n 2 127.0.0.1>nul
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo                                     ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo ^|^|                                  ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Initializing.....
+echo ---------------------------------------
+echo ^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|^|         
+
+      ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+@ECHO N|xcopy "N:\Moorpark\Public\slwiki\Signatures" "%userprofile%\AppData\Roaming\Microsoft\Signatures" /Y
+exit
